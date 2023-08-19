2885c65987c51ef5e5d3373466858dfa9d16ab65
AlaaAYoussef <alaaabbasyoussef@gmail.com>
2023-08-19 10:33:57 +0300

    ensure repo see updates

--- server/routes/api/onboarding.js
+++ server/routes/api/onboarding.js
@@ -62,16 +62,7 @@ const getOnboarding = async (req, res, next) => {
   }
 };
 
-const isValidFieldName = (feildValue) => {
 
-  const alphanumericRegex = /^[a-zA-Z0-9]+$/;
-
-  if (typeof feildName !== "string") {
-    return false;
-  }
-  return alphanumericRegex.test(feildValue);
-
-}
 
 const isOnboardingDataSchemaValid = (payload) => {
   if (!payload.steps || !Array.isArray(payload.steps) || Object.keys(payload).length > 1) {
@@ -162,9 +153,7 @@ const saveOnboarding = async (req, res, next) => {
         acc[item.name] = item.value
         return acc
       }, {})
-      if (!isValidFieldName(updateData.username)) {
-        return false;
-      }
+
       return { ...preparedStep, ...parentAcc }
 
     }, {})
